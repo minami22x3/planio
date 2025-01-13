7d2daaacf4a71d46f7af068c6f72cd08fef02b22
bachletantien <mrbanhu147@gmail.com>
2025-01-13 20:45:08 +0700

    update code

---
+++ components/ui/checkbox.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { Check } from 'lucide-react';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root ref={ref} className={cn('checkbox peer', className)} {...props}>
+    <CheckboxPrimitive.Indicator className={cn('text-current flex items-center justify-center')}>
+      <Check className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
